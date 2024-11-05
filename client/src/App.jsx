193b78794bea0bc1859c34c32cca3fdbbeb62404
Rizwan Ali Ansari <rizwan459@gmail.com>
2024-11-06 01:15:04 +0500

    Night Work 6-11-24

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import About from './pages/About'
 import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
 
 export default function App() {
   return (
     
     <BrowserRouter>
+<Header/>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
