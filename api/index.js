59f3dcf5cea459f92da2cebdc00374601d108c8b
Rizwan Ali Ansari <rizwan459@gmail.com>
2024-11-08 18:01:34 +0500

    test api

--- api/index.js
+++ api/index.js
@@ -20,3 +20,8 @@ console.log(err);
  app.listen (3000, () => {
     console.log('Server is running on port 3000!!');
  });
+
+ app.get('/test', (req, res) => {
+   res.json({message: 'API is woring'})
+ }
+);
